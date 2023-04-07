7f58f31b89537ce9494b67ff067bc861339f3ab1
məhəmməd <126715828+MagaOruc@users.noreply.github.com>
2023-04-07 13:49:22 +0400

    Add files via upload

---
+++ testt/main.js
@@ -0,0 +1,35 @@
+ let myName ="Mahammad"
+ 
+ for (let i=0;i< myName.length;i++){
+console.log(myName[i])
+
+}
+
+let m=0
+for (; m<=8; m++)
+
+{if (m%2==0)
+    console.log(m)
+}
+ 
+    
+
+
+
+
+
+
+
+
+
+
+
+let k=0
+for( ; k <=8; k++ ){
+     if( k%2== 0 ){
+        console.log( `${k} is even number`)
+        
+        
+     }
+     else{ console.log( `${k} is odd number`) }
+}
